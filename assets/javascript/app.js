4cb83ff3870a5e854fe52cf01c6ee983bbd1017b
Belle Chauviray <bebold.bebelle@gmail.com>
2018-06-12 00:42:13 -0400

    Created my variable containing all my trivia questions and answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,43 @@
-console.log("This is to confirm my JS file is linked");
+// console.log("This is to confirm my JS file is linked");
 
 $(document).ready(function () {
 
-    $(".btn").on("click",function() {
-        console.log("JQ button works");
-        // var test =$("<div>").addClass("test","test2").append($("<h1>").text("this is my first appended title").append("<h2>").text("this is my second appended title"));
-        // $("#lead").append(test);
-    })
+var myQuestions = {
+    questionOne:{
+        question: "What is the tradition behind the onion soup?",
+        answers:["The soup is brought in bed at night to the newly weds.", "answer2", "answer3", "answer4"],
+    },
+    questionTwo:{
+        question: "As it is often times reminded to the Frnehc,  which war did they surrender?",
+        answers:["answer1", "answer2", "answer3", "answer4"]
+    },
+    questionThree:{
+        question: "Which of these electro band is French?",
+        answers:["answer1", "answer2", "answer3", "answer4"]
+    },
+    questionFour:{
+        question: "what nikname was given to us by the Brits?",
+        answers:["answer1", "answer2", "answer3", "answer4"]
+    },
+    questionFive:{
+        question: "French are know for eat:",
+        answers:["answer1", "answer2", "answer3", "answer4"]
+    },
+    questionSix:{
+        question: "which of these cheeses is not a French cheese?:",
+        answers:["answer1", "answer2", "answer3", "answer4"]
+    },
+}  
+    
 
-// * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+// * User clicks "Start" to begin the game and first question displays.
 
-// * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
-
-// * The scenario is similar for wrong answers and time-outs.
+// * Each questions display a 30 seconds timer and four possible answers.
+// * If answer is incorrect then
 
 //   * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
 //   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
 
 // * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
 
-})
+}
